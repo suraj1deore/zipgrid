d3af6d637363c049c4f14b08331755e7769041c2
suraj1deore <100829348+suraj1deore@users.noreply.github.com>
2022-03-04 13:47:25 +0530

    Update settings.py

--- zipgrid_test/settings.py
+++ zipgrid_test/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-e)v2*(f98k!c9f@5f_1*2janlhj$e48o%2w*h#6mz8t!o^^!u!'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUGE', default=True, cast=bool)
+DEBUG = True
 
 
 ALLOWED_HOSTS = []
